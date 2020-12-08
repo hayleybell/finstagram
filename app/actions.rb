ce5949b7739a36fc5666111430db7e76af0ad39e
hayleybell <hayleybell@live.ca>
2020-12-08 04:22:58 +0000

    add controller logic for user login; add associated view

--- app/actions.rb
+++ app/actions.rb
@@ -1,24 +1,59 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
+# homepage (index)
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
 
+# signup page
 get '/signup' do
   @user = User.new
   erb(:signup)
 end
 
+# signup a user
 post '/signup' do
-  email      = params[:email]
+  email = params[:email]
   avatar_url = params[:avatar_url]
-  username   = params[:username]
-  password   = params[:password]
+  username = params[:username]
+  password = params[:password]
 
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
   
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
+end
+
+# login form
+get '/login' do
+    erb(:login)
+end
+
+# login a user
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
