d56ffa2d38433147468d05b27fbc6ff5601c1dda
hayleybell <hayleybell@live.ca>
2020-12-18 02:35:59 +0000

    pre deployment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
